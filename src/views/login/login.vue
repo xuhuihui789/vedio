46d24a9d80820b8f67954bb85feccbfea3c5bca7
xuhuihui <xuhuihui@flyyou.me>
2019-11-07 10:40:02 +0800

    update

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -13,118 +13,124 @@
 				videoInput:'',//video设备
 				localVideo:'',
 				streamId:'',
-            }
-        },
-        methods:{
-	        //	获取登录 token
-	        getLoginToken() {
-	        	let params = {
-			        appId:this.roomConfig.liveRoomConfig.appId,
-			        appKey:this.roomConfig.liveRoomConfig.signKey,
-			        idName:'test'
-                }
-		        this.$axios.get(this.URL.getToken.token,{params}).then(res => {
-			        this.loginToken = res
-			        this.login()
-		        })
-	        },
-	        //   登录
-	        login() {
-		        this.zg.onDisconnect = function (err) {
-			        //网络断开后的回调处理
-		        };
+			}
+		},
+		methods:{
+			//	获取登录 token
+			getLoginToken() {
+				let params = {
+					appId:this.roomConfig.liveRoomConfig.appId,
+					appKey:this.roomConfig.liveRoomConfig.signKey,
+					idName:'test'
+				}
+				this.$axios.get(this.URL.getToken.token,{params}).then(res => {
+					this.loginToken = res
+					this.login()
+				})
+			},
+			//   登录
+			login() {
+				this.zg.onDisconnect = function (err) {
+					//网络断开后的回调处理
+				};
 
-		        this.zg.onKickOut = function (err) {
-			        //踢出房间后的回调处理
-		        };
-		        let that = this
-		        this.zg.onGetAnchorInfo = function (anchor_userid, anchro_username) {
-			        //主播信息更新回调处理，在登录成功后触发
-			        that.zg.enumDevices(function(deviceInfo){
-				        console.log(deviceInfo.microphones);
-				        console.log(deviceInfo.cameras);
-				        if (deviceInfo.microphones) {
-					        for (let i = 0; i < deviceInfo.microphones.length; i++) {
-						        console.log(deviceInfo.microphones[i].deviceId);
-					        }
-					        //	TODO 赋值 _this.audioInput
-				        }
-				        if (deviceInfo.cameras) {
-					        for (let j = 0; j < deviceInfo.cameras.length; j++) {
-						        console.log(deviceInfo.cameras[i].deviceId);
-					        }
-					        //	TODO 赋值_this.videoInput
-				        }
-				        // 开始预览
-				        that.doPreview('default',deviceInfo.cameras[0].deviceId)
-			        },function(error){
-				        //媒体设备信息回调失败
-			        })
-		        };
-		        // let that = this;
-		        this.zg.login("111", 1, this.loginToken, function (streamList) {
-			     //    // //枚举设备
-			     //    let _that = that
-			     //    that.zg.enumDevices(function(deviceInfo){
-				 //        console.log(deviceInfo.microphones);
-				 //        console.log(deviceInfo.cameras);
-				 //        if (deviceInfo.microphones) {
-					//         for (let i = 0; i < deviceInfo.microphones.length; i++) {
-					// 	        console.log(deviceInfo.microphones[i].deviceId);
-					//         }
-					//         //	TODO 赋值 _this.audioInput
-				 //        }
-				 //        if (deviceInfo.cameras) {
-					//         for (let j = 0; j < deviceInfo.cameras.length; j++) {
-					// 	        console.log(deviceInfo.cameras[i].label);
-					//         }
-					//         //	TODO 赋值_this.videoInput
-				 //        }
-				 //        // 开始预览
-				 //        _that.doPreview('default',deviceInfo.cameras[0].deviceId)
-			     //    },function(error){
-				 //        //媒体设备信息回调失败
-			     //    })
-		        }, function (err) {
-			        console.log(err);
-		        })
-	        },
-	        //   本地预览  使用默认设备时，audioInput和videoInput参数不用填
-	        doPreview(audioInput,videoInput){
-		        let avConstraints = {
-			        audio: true,//是否需要音频
-			        audioInput: audioInput,//	麦克风设备Id
-			        video: true,// 是否需要视频
-			        videoInput: videoInput,//  摄像头设备Id
-			        videoQuality: 4, //视频质量等级
-			        horizontal: true,//是否横屏视频
-			        externalCapture:true,//是否使用外部来源
-			        // externalMediaStream:'',//外部提供MediaStram对象	 mediaStreamConstraints增加 width / height / frameRate / bitRate 属性，需要把这四个属性也传递给SDK
-			        noiseSuppression:true,//是否开启降噪
-			        autoGainControl:true,//	是否开启自动增益
-			        echoCancellation:true,//是否开启回声消除
-		        };
-		        //WebRTC可以共用相同设备，所以预览时必须指定开播的streamId
-                this.localVideo = this.$refs.videoTeacher
-		        console.log(this.localVideo);
-		        let _this = this
-		        this.zg.startPreview(this.localVideo, avConstraints, function(success) {
-			        //成功开播
-			        _this.doPublish();
-		        }, function(error) {
-			        alert("start device error " , error);
-		        });
-	        },
-	        //   开播
-	        doPublish(){
-	        	alert(111)
-		        let result = this.zg.startPublishingStream('abc', this.localVideo);
-		        console.log(result);
-		        if (!result) {
-			        alert("publish " + publishStreamId + " return " + result);
-		        }
-	        }
-        },
+				this.zg.onKickOut = function (err) {
+					//踢出房间后的回调处理
+				};
+				let that = this
+				this.zg.onGetAnchorInfo = function (anchor_userid, anchro_username) {
+					//主播信息更新回调处理，在登录成功后触发
+					that.zg.enumDevices(function(deviceInfo){
+						if (deviceInfo.microphones) {
+							for (let i = 0; i < deviceInfo.microphones.length; i++) {
+								console.log(deviceInfo.microphones[i].deviceId);
+							}
+							//	TODO 赋值 _this.audioInput
+						}
+						if (deviceInfo.cameras) {
+							for (let j = 0; j < deviceInfo.cameras.length; j++) {
+								// console.log(deviceInfo.cameras[j].label);
+							}
+							//	TODO 赋值_this.videoInput
+						}
+						// 开始预览
+						that.doPreview('default',deviceInfo.cameras[0].deviceId)
+					},function(error){
+						//媒体设备信息回调失败
+					})
+				};
+				// let that = this;
+				this.zg.login("111", 1, this.loginToken, function (streamList) {
+					console.log(streamList);
+					// that.doPreview()
+					// //枚举设备
+					/* let _that = that
+                     that.zg.enumDevices(function(deviceInfo){
+                         console.log(deviceInfo.microphones);
+                         console.log(deviceInfo.cameras);
+                         if (deviceInfo.microphones) {
+                             for (let i = 0; i < deviceInfo.microphones.length; i++) {
+                                 console.log(deviceInfo.microphones[i].deviceId);
+                                 // _this.audioInput = deviceInfo.microphones[0].deviceId
+                                 // console.log(_this.audioInput);
+                             }
+                             //	TODO 赋值 _this.audioInput
+                         }
+                         if (deviceInfo.cameras) {
+                             for (let j = 0; j < deviceInfo.cameras.length; j++) {
+                                 console.log(deviceInfo.cameras[i].label);
+                             }
+
+                             //	TODO 赋值_this.videoInput
+                         }
+                         // 开始预览
+                         _that.doPreview('default',deviceInfo.cameras[0].deviceId)
+                     },function(error){
+                         //媒体设备信息回调失败
+                     })*/
+				}, function (err) {
+					console.log(err);
+				})
+			},
+			//   本地预览  使用默认设备时，audioInput和videoInput参数不用填
+			doPreview(audioInput,videoInput){
+				alert(111)
+				let avConstraints = {
+					audio: true,//是否需要音频
+					audioInput: audioInput,//	麦克风设备Id
+					video: true,// 是否需要视频
+					videoInput: videoInput,//  摄像头设备Id
+					videoQuality: 1, //视频质量等级
+					horizontal: true,//是否横屏视频
+					externalCapture:true,//是否使用外部来源
+					// externalMediaStream:'',//外部提供MediaStram对象	 mediaStreamConstraints增加 width / height / frameRate / bitRate 属性，需要把这四个属性也传递给SDK
+					// noiseSuppression:true,//是否开启降噪
+					// autoGainControl:true,//	是否开启自动增益
+					// echoCancellation:true,//是否开启回声消除
+				};
+				//WebRTC可以共用相同设备，所以预览时必须指定开播的streamId
+				this.localVideo = this.$refs.videoTeacher
+				console.log(this.localVideo);
+				let _this = this
+				this.zg.startPreview(this.localVideo, avConstraints, function(success) {
+					//成功开播
+					alert(222)
+					_this.doPublish();
+				}, function(error) {
+					alert("start device error ");
+				});
+				// this.doPublish();
+			},
+			//   开播
+			doPublish(){
+				alert(333)
+				let result = this.zg.startPublishingStream('abc', this.localVideo);
+				console.log(result);
+				if (!result) {
+					alert("publish " + publishStreamId + " return " + result);
+				}
+			}
+		},
 		mounted(){
 			// 配置必要参数
 			let config = {
@@ -140,8 +146,8 @@
 			}
 			this.zg.config(config);
 			this.getLoginToken()
-        }
-    }
+		}
+	}
 </script>
 
 <style lang="less">
